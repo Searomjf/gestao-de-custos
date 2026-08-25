5f16b8064776f3a81b0cade50ef3a9888a18d59d
Searomjf <jefersonmoraesdf@gmail.com>
2026-08-25 19:47:27 -0300

    Adiciona next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/gestao-de-custos',
+  assetPrefix: '/gestao-de-custos/',
+  trailingSlash: true,
+};
+
+export default nextConfig;
+
